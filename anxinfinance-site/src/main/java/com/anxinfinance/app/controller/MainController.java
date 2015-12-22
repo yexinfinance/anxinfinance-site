b92a91cc9109a234c73e0e302a3337d7f31dab76
rainedream <raineddream@gmail.com>
2015-12-22 20:14:14 +0800

    Build web container with Maven

---
+++ anxinfinance-site/src/main/java/com/anxinfinance/app/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.anxinfinance.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(path = "/index", method = GET)
+    public String index(Model model) {
+        return "main";
+    }
+}
