9c7e5cd883d2371b4752af385f9ed88188434d06
rainedream <raineddream@gmail.com>
2016-01-04 15:47:57 +0800

    Disable thymeleaf cache when reload static resources in DEV env

---
+++ anxinfinance-site/src/main/java/com/anxinfinance/app/Constrants.java
@@ -0,0 +1,7 @@
+package com.anxinfinance.app;
+
+public final class Constrants {
+
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+
+}
---
+++ anxinfinance-site/src/main/java/com/anxinfinance/app/ThymeleafConfiguration.java
@@ -0,0 +1,33 @@
+package com.anxinfinance.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import javax.inject.Inject;
+
+@Configuration
+@Profile(Constrants.SPRING_PROFILE_DEVELOPMENT)
+public class ThymeleafConfiguration {
+
+    @Inject
+    private ThymeleafProperties properties;
+
+    @Value("${spring.thymeleaf.templates_root:}")
+    private String templatesRoot;
+
+    @Bean
+    public ITemplateResolver defaultTemplateResolver() {
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setSuffix(properties.getSuffix());
+        resolver.setPrefix(templatesRoot);
+        resolver.setTemplateMode(properties.getMode());
+        resolver.setCharacterEncoding(String.valueOf(properties.getEncoding()));
+        resolver.setCacheable(properties.isCache());
+
+        return resolver;
+    }
+}
